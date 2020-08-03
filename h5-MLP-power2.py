5e1d8269aeaea4470d796b06dadd973bde1214f2
PC <howsam.mail@gmail.com>
2020-08-03 23:24:44 +0430

    y=x**2

--- h5-MLP-power2.py
+++ h5-MLP-power2.py
@@ -6,12 +6,13 @@ x_size = x.size()
 #x = torch.transpose(x,1)
 #x =x.t()
 y = torch.tensor([[0.0], [1.0], [4.0], [9.0], [16.0], [25.0], [36.0], [49.0], [64.0], [81.0], [100.0]])
+# use dataloader for batch data loading
 
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.linear1 = torch.nn.Linear(1,2)
-        self.linear2 = torch.nn.Linear(2,1)
+        self.linear1 = torch.nn.Linear(1,10) # play with number of neurons and mlp layers
+        self.linear2 = torch.nn.Linear(10,1)
         self.relu = torch.nn.ReLU()
 
     def forward(self,x):
@@ -21,18 +22,23 @@ class Model(torch.nn.Module):
         return y3
 
 model = Model()
+model.train()  # set the model to training mode
 
-loss_fn = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(),lr=0.01)
+loss_fn = torch.nn.MSELoss(reduction='mean')  # use mean mode instead of sum
+optimizer = torch.optim.SGD(model.parameters(),lr=0.001)  # play with learning rate and optimizer, important!
 
 print("start")
-for i in range(500):
+for i in range(10000):  # play with number of epochs
     y_pred = model(x)
     loss = loss_fn(y_pred,y)
     print(i, loss.item())
-    optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+    optimizer.zero_grad()
 
         
-    
+model.eval()  # set the model to eval mode for test
+with torch.no_grad():  # no need to calc gradients in evaluation mode
+    x_test = torch.tensor([[8.5]])
+    yp = model(x_test)
+    print('prediction: {}, target: {}'.format(yp, x_test**2))
